845fad1759f183b7b7ec949c80ded007d8318ed1
Srikanth2304 <59932476+Srikanth2304@users.noreply.github.com>
2023-08-22 21:12:51 +0530

    Create axessspringboottraining.java

---
+++ axessspringboottraining.java
@@ -0,0 +1,13 @@
+package com.scb.axessspringboottraining;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AxessSpringbootTrainingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AxessSpringbootTrainingApplication.class, args);
+	}
+
+}
